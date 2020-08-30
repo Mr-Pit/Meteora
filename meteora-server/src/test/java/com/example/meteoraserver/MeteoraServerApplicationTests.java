51f7a182ad1cac4ff2367d56be6b7117afdd7807
Pitney A. Jules <43119655+Mr-Pit@users.noreply.github.com>
2020-08-30 15:58:54 -0400

    Backend JAVA Server Added

---
+++ meteora-server/src/test/java/com/example/meteoraserver/MeteoraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meteoraserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
